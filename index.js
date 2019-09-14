ec5978ccef1e235cea99fc3e8a5523085b129802
Joy Qi <joy_huanqi@outlook.com>
2019-09-14 17:34:18 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// ReactDOM.render(WHAT DO I WANT TO RENDER, WHERE DO I WANT TO RENDER IT)
+ReactDOM.render(<div><h1>Hello world!</h1><p> This is a paragraph</p></div>, document.getElementById("root"))
+// JSX Javascript version of HTML
+// Old way 
+// var myNewP = document.createElement("p")
+// myNewP.innerHTML = "This is a paragraph."
